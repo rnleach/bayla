2ae5036f42c3e7d016272274926e341d93309137
Ryan <rnleach@users.noreply.github.com>
2025-06-09 10:51:36 -0600

    Add preconditioning to the known integral tests.

--- test/integrator_tests.c
+++ test/integrator_tests.c
@@ -197,7 +197,7 @@ test_integrator_pi(void)
 }
 
 /*-------------------------------------------------   Known Integral  ----------------------------------------------------*/
-#define KI_RANGE 50
+#define KI_RANGE 100
 
 f64 ki_min_parms[] = { -KI_RANGE, -KI_RANGE, -KI_RANGE, -KI_RANGE, -KI_RANGE, -KI_RANGE, -KI_RANGE };
 f64 ki_max_parms[] = {  KI_RANGE,  KI_RANGE,  KI_RANGE,  KI_RANGE,  KI_RANGE,  KI_RANGE,  KI_RANGE };
@@ -344,22 +344,117 @@ test_integrator_ki(void)
 
     _Static_assert(ECO_ARRAY_SIZE(opts) == ECO_ARRAY_SIZE(names), "Mismatched Arrays.");
 
-    TestEvidenceKiThreadData tds[ECO_ARRAY_SIZE(opts)] = {0};
-    CoyThread threads[ECO_ARRAY_SIZE(opts)] = {0};
-    MagStaticArena scratches[ECO_ARRAY_SIZE(opts)] = {0};
-    for(size method = 0; method < ECO_ARRAY_SIZE(opts); ++method)
+    char *precond_names[] = { "Precond-VEGAS", "Precond-MISER-VEGAS", "Precond-VEGAS+"};
+
+    BayLaIntegratorOptions precond_opts[]= 
+        {
+            (BayLaIntegratorOptions)
+            {
+                .strategy = BAYLA_INTEGRATE_VEGAS,
+                .vegas.min_total_samples = 10000,
+                .vegas.max_total_samples = 20000000,
+                .vegas.samples_per_refinement = 10000,
+                .vegas.num_grid_cells = 1000,
+                .vegas.acceptable_abs_error = -1.0e-10,
+                .vegas.acceptable_prop_error = 0.0001,
+                .vegas.alpha = 0.75,
+                .vegas.seed = 45
+            },
+
+            (BayLaIntegratorOptions)
+            {
+                .strategy = BAYLA_INTEGRATE_VEGAS_MISER,
+                .vegas_miser.min_total_samples = 10000,
+                .vegas_miser.max_total_samples = 20000000,
+                .vegas_miser.samples_per_refinement = 10000,
+                .vegas_miser.num_grid_cells = 1000,
+                .vegas_miser.min_points = 30,
+                .vegas_miser.min_to_subdivide = 120,
+                .vegas_miser.explore_factor = 0.1,
+                .vegas_miser.acceptable_abs_error = -1.0e-10,
+                .vegas_miser.acceptable_prop_error = 0.0001,
+                .vegas_miser.alpha = 0.75,
+                .vegas_miser.seed = 46
+            },
+
+            (BayLaIntegratorOptions)
+            {
+                .strategy = BAYLA_INTEGRATE_VEGAS_PLUS,
+                .vegas_plus.min_total_samples = 10000,
+                .vegas_plus.max_total_samples = 20000000,
+                .vegas_plus.samples_per_refinement = 10000,
+                .vegas_plus.num_grid_cells = 1000,
+                .vegas_plus.acceptable_abs_error = -1.0e-10,
+                .vegas_plus.acceptable_prop_error = 0.0001,
+                .vegas_plus.alpha = 0.75,
+                .vegas_plus.beta = 0.75,
+                .vegas_plus.seed = 47
+            }
+        };
+
+    _Static_assert(ECO_ARRAY_SIZE(precond_opts) == ECO_ARRAY_SIZE(precond_names), "Mismatched precond-Arrays.");
+
+    TestEvidenceKiThreadData tds[ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts)] = {0};
+    CoyThread threads[ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts)] = {0};
+    MagStaticArena scratches[ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts)] = {0};
+    MagStaticArena perms[ECO_ARRAY_SIZE(precond_opts)] = {0};
+    for(size method = 0; method < ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts); ++method)
     {
         scratches[method] = mag_static_arena_allocate_and_create(ECO_KiB(512));
+
+        if(method >= ECO_ARRAY_SIZE(opts))
+        {
+            size other_idx = method - ECO_ARRAY_SIZE(opts);
+            perms[other_idx] = mag_static_arena_allocate_and_create(ECO_MiB(32));
+        }
     }
     
+    ElkRandomState ran = elk_random_state_create(111);
     for(size np = 2; np <= ECO_ARRAY_SIZE(ki_min_parms); ++np)
     {
+        /* Do the preconditioning. */
+        for(size other_idx = 0; other_idx < ECO_ARRAY_SIZE(precond_opts); ++other_idx)
+        {
+            eco_reset(&perms[other_idx]);
+            BayLaModel model = ki_model;
+            model.n_parameters = np;
+
+            f64 starting_values[ECO_ARRAY_SIZE(ki_min_parms)] = {0};
+            f64 *min_vals = model.min_parameter_vals;
+            f64 *max_vals = model.max_parameter_vals;
+            for(size n = 0; n < model.n_parameters; ++n)
+            {
+                starting_values[n] = (min_vals[n] + max_vals[n]) / 2.0;
+            }
+
+            BayLaPreconditioningSamplerArgs args = bayla_preconditioning_sampler_args_allocate_and_create(
+                    &model,
+                    1000,
+                    10000,
+                    starting_values,
+                    &perms[other_idx]);
+
+            bayla_preconditioning_sample(&args, &ran, mag_static_arena_borrow(&scratches[0]));
+
+            BayLaParametersSamples *samples = args.output;
+            bayla_vegas_map_precondition(&model, &precond_opts[other_idx], samples, &perms[other_idx]);
+        }
+
         b32 success = true;
-        for(size method = 0; method < ECO_ARRAY_SIZE(opts); ++method)
+        for(size method = 0; method < ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts); ++method)
         {
             tds[method].model = ki_model;
             tds[method].model.n_parameters = np;
-            tds[method].opts = opts[method];
+
+            if(method < ECO_ARRAY_SIZE(opts))
+            {
+                tds[method].opts = opts[method];
+            }
+            else
+            {
+                tds[method].opts = precond_opts[method - ECO_ARRAY_SIZE(opts)];
+            }
+
             tds[method].scratch = mag_static_arena_borrow(&scratches[method]);
 
             success &= coy_thread_create(&threads[method], test_evidence_ki_thread_func, &tds[method]);
@@ -367,17 +462,27 @@ test_integrator_ki(void)
         }
 
         printf("\nCalculating a known integral with %ld parameters as a test of the integrators.\n", np);
-        printf("%20s - %10s ± %8s [%9s] %16s [%9s] %8s [ %8s ]\n",
+        printf("%20s - %10s ± %9s [%9s] %16s [%10s] %8s [ %8s ]\n",
                 "Method", "Value", "Error", "Pct Err", "True Err Z score", "Pct True", "Samples", "Samples Used");
 
-        for(size method = 0; method < ECO_ARRAY_SIZE(opts); ++method)
+        for(size method = 0; method < ECO_ARRAY_SIZE(opts) + ECO_ARRAY_SIZE(precond_opts); ++method)
         {
             success = coy_thread_join(&threads[method]);
             Assert(success);
             coy_thread_destroy(&threads[method]);
 
-            printf("%20s - %10lf ± %8lf [%8.4lf%%] %16.4lf [%8.4lf%%]",
-                    names[method],
+            char *name = NULL;
+            if(method < ECO_ARRAY_SIZE(opts))
+            {
+                name = names[method];
+            }
+            else
+            {
+                name = precond_names[method - ECO_ARRAY_SIZE(opts)];
+            }
+
+            printf("%20s - %10lf ± %9lf [%8.4lf%%] %16.4g [%9.4lf%%]",
+                    name,
                     bayla_log_value_map_out_of_log_domain(tds[method].evidence.result.value), 
                     bayla_log_value_map_out_of_log_domain(tds[method].evidence.result.error),
                     100.0 * bayla_log_value_map_out_of_log_domain(bayla_log_value_divide(
