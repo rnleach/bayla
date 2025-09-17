00c6e8897074636f46a35d68edbedd8c65927554
Ryan <rnleach@users.noreply.github.com>
2025-09-17 12:38:15 -0600

    Fourth order model added.

--- make_plot.plt
+++ make_plot.plt
@@ -5,17 +5,46 @@ set style fill solid
 
 # Test Data ----------------------------------------------------------------------------------------------------------------
 fn = "test_data.csv"
+set term qt 0 size 1600,800
 
 set key left
 set xrange [0.09:3.01]
-plot fn using 1:2 with points pt 7 title "Raw Data"
-replot log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 4 dt 2 title "True Function"
-replot log(x) lw 2 title "Log Model"
-replot 7.180454e-01 lw 2 title "Constant"
-replot 1.409042e+00 * x + -1.465970e+00 lw 2 title "Linear"
-replot 2.848551e-01 * x**2 + 5.259913e-01 * x + -9.914793e-01 lw 2 title "Second Order"
-replot 4.245637e-01 * x**3 + -1.689366e+00 * x**2 + 3.023451e+00 * x + -1.700497e+00 lw 2 title "Third Order"
 
+set origin 0,0
+set size 2,4
+set multiplot layout 2,4 rowsfirst scale 0.9, 0.9 title "Test Data"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     log(x) lw 3 title "Log Model"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     7.180454e-01 lw 3 title "Constant"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     1.409042e+00 * x + -1.465970e+00 lw 3 title "Linear"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     2.848551e-01 * x**2 + 5.259913e-01 * x + -9.914793e-01 lw 3 title "Second Order"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     4.245637e-01 * x**3 + -1.689366e+00 * x**2 + 3.023451e+00 * x + -1.700497e+00 lw 3 title "Third Order"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function", \
+     -3.651546e-05 * x**4 + 4.348583e-01 * x**3 + -1.742574e+00 * x**2 + 3.101399e+00 * x + -1.727254e+00 lw 3 title "Fourth Order"
+
+plot fn using 1:2 with points pt 7 title "Raw Data", \
+     log(0.9) + (x - 0.9) / 0.9 - (x - 0.9)**2 / (2 * 0.9**2) + (x -  0.9)**3 / (3 * 0.9**3) lw 2 dt 2 title "True Function"
+
+unset multiplot
 unset xrange
 
 # Simple Model -------------------------------------------------------------------------------------------------------------
@@ -196,6 +225,69 @@ plot fn using 3:5:(exp($8)) with points palette pt 7 notitle
 plot fn using 4:5:(exp($8)) with points palette pt 7 notitle
 set title "sigma"
 plot fn using (bin($5, binwidth)):(exp($8)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+
+unset multiplot
+
+# Fourth Order Model --------------------------------------------------------------------------------------------------------
+fn = "fourth_order_model.csv"
+set term qt 7 size 1000,800
+
+set origin 0,0
+set size 1,1
+set multiplot layout 6,6 rowsfirst scale 1.0, 1.0 title "Fourth Order Model Test"
+
+set title "V0"
+plot fn using (bin($1, binwidth)):(exp($9)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+set multiplot next
+set multiplot next
+set multiplot next
+set multiplot next
+set multiplot next
+
+unset title
+plot fn using 1:2:(exp($9)) with points palette pt 7 notitle
+set title "b"
+plot fn using (bin($2, binwidth)):(exp($9)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+set multiplot next
+set multiplot next
+set multiplot next
+set multiplot next
+
+unset title
+plot fn using 1:3:(exp($9)) with points palette pt 7 notitle
+plot fn using 2:3:(exp($9)) with points palette pt 7 notitle
+set title "c"
+plot fn using (bin($3, binwidth)):(exp($8)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+set multiplot next
+set multiplot next
+set multiplot next
+
+unset title
+plot fn using 1:4:(exp($9)) with points palette pt 7 notitle
+plot fn using 2:4:(exp($9)) with points palette pt 7 notitle
+plot fn using 3:4:(exp($9)) with points palette pt 7 notitle
+set title "d"
+plot fn using (bin($4, binwidth)):(exp($9)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+set multiplot next
+set multiplot next
+
+unset title
+plot fn using 1:5:(exp($9)) with points palette pt 7 notitle
+plot fn using 2:5:(exp($9)) with points palette pt 7 notitle
+plot fn using 3:5:(exp($9)) with points palette pt 7 notitle
+plot fn using 4:5:(exp($9)) with points palette pt 7 notitle
+set title "e"
+plot fn using (bin($5, binwidth)):(exp($9)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
+set multiplot next
+
+unset title
+plot fn using 1:6:(exp($9)) with points palette pt 7 notitle
+plot fn using 2:6:(exp($9)) with points palette pt 7 notitle
+plot fn using 3:6:(exp($9)) with points palette pt 7 notitle
+plot fn using 4:6:(exp($9)) with points palette pt 7 notitle
+plot fn using 5:6:(exp($9)) with points palette pt 7 notitle
+set title "sigma"
+plot fn using (bin($6, binwidth)):(exp($9)) smooth kdensity bandwidth binwidth with l lw 4 lc 19 notitle
 
 unset multiplot
 
--- test/evidence_test.c
+++ test/evidence_test.c
@@ -606,20 +606,20 @@ static f64
 third_order_model_log_prior(size num_parms, f64 const *parms, void *user_data)
 {
     f64 v0 = parms[0];
-    f64 v0_min = second_order_min_parms[0];
-    f64 v0_max = second_order_max_parms[0];
+    f64 v0_min = third_order_min_parms[0];
+    f64 v0_max = third_order_max_parms[0];
 
     f64 b = parms[1];
-    f64 b_min = second_order_min_parms[1];
-    f64 b_max = second_order_max_parms[1];
+    f64 b_min = third_order_min_parms[1];
+    f64 b_max = third_order_max_parms[1];
 
     f64 c = parms[2];
-    f64 c_min = second_order_min_parms[2];
-    f64 c_max = second_order_max_parms[2];
+    f64 c_min = third_order_min_parms[2];
+    f64 c_max = third_order_max_parms[2];
 
     f64 d = parms[3];
-    f64 d_min = second_order_min_parms[3];
-    f64 d_max = second_order_max_parms[3];
+    f64 d_min = third_order_min_parms[3];
+    f64 d_max = third_order_max_parms[3];
     
     f64 sigma = parms[4];
     f64 sigma_min = third_order_min_parms[4];
@@ -813,6 +813,255 @@ BayLaModel third_order_model =
         .user_data = &third_order_user_data
     };
 
+/*-----------------------------------------------  4th Order Polynomial  -------------------------------------------------*/
+/* param 0 is constant, param 1 is linear, param 2 is quadratic , parm 3 is cubic, parm 4 is quartic, param 5 is a
+ * standard deviation. */
+f64 fourth_order_min_parms[6] = {-4.0, -4.0, -4.0, -4.0, -4.0, log_model_min_sigma };
+f64 fourth_order_max_parms[6] = {+4.0, +4.0, +4.0, +4.0, +4.0, log_model_max_sigma };
+
+static f64 
+fourth_order_model_log_prior(size num_parms, f64 const *parms, void *user_data)
+{
+    f64 v0 = parms[0];
+    f64 v0_min = fourth_order_min_parms[0];
+    f64 v0_max = fourth_order_max_parms[0];
+
+    f64 b = parms[1];
+    f64 b_min = fourth_order_min_parms[1];
+    f64 b_max = fourth_order_max_parms[1];
+
+    f64 c = parms[2];
+    f64 c_min = fourth_order_min_parms[2];
+    f64 c_max = fourth_order_max_parms[2];
+
+    f64 d = parms[3];
+    f64 d_min = fourth_order_min_parms[3];
+    f64 d_max = fourth_order_max_parms[3];
+    
+    f64 e = parms[4];
+    f64 e_min = fourth_order_min_parms[4];
+    f64 e_max = fourth_order_max_parms[4];
+    
+    f64 sigma = parms[5];
+    f64 sigma_min = fourth_order_min_parms[5];
+    f64 sigma_max = fourth_order_max_parms[5];
+    
+    if(
+            sigma < sigma_min || sigma > sigma_max 
+            || v0 < v0_min || v0 > v0_max
+            || b < b_min || b > b_max
+            || c < c_min || c > c_max
+            || d < d_min || d > d_max
+            || e < e_min || e > e_max
+      )
+    {
+        return -INFINITY;
+    }
+
+    f64 zc = (v0_max - v0_min) * (b_max - b_min) * (c_max - c_min) * (d_max - d_min) * (e_max - e_min) * log(sigma_max / sigma_min);
+    return -log(zc * sigma);
+}
+
+static f64 
+fourth_order_model_log_likelihood(size num_parms, f64 const *parms, void *user_data)
+{
+    f64 v0 = parms[0];
+    f64 b = parms[1];
+    f64 c = parms[2];
+    f64 d = parms[3];
+    f64 e = parms[4];
+    f64 sigma = parms[5];
+
+    UserData *data = user_data;
+    f64 N = (f64)data->N;
+
+    f64 x_bar = data->x_bar;
+    f64 x_sq_bar = data->x_sq_bar;
+    f64 x3_bar = data->x3_bar;
+    f64 x4_bar = data->x4_bar;
+    f64 x5_bar = data->x5_bar;
+    f64 x6_bar = data->x6_bar;
+    f64 x7_bar = data->x7_bar;
+    f64 x8_bar = data->x8_bar;
+
+    f64 y_bar = data->y_bar;
+    f64 y_sq_bar = data->y_sq_bar;
+
+    f64 xy_bar = data->xy_bar;
+    f64 y_x2_bar = data->y_x2_bar;
+    f64 y_x3_bar = data->y_x3_bar;
+    f64 y_x4_bar = data->y_x4_bar;
+
+    f64 Q = v0 * v0 + b * b * x_sq_bar + c * c * x4_bar + d * d * x6_bar + e * e * x8_bar + y_sq_bar +
+            2.0 * (v0 * b * x_bar + v0 * c * x_sq_bar + v0 * d * x3_bar + v0 * e * x4_bar
+                   + b * c * x3_bar + b * d * x4_bar + b * e * x5_bar
+                   + c * d * x5_bar + c * e * x6_bar 
+                   + d * e * x7_bar
+                   - v0 * y_bar - b * xy_bar - c * y_x2_bar - d * y_x3_bar - e * y_x4_bar);
+
+    data->ncalls++;
+
+    return -N * 0.5 * log(ELK_2_PI) - N * log(sigma) - 0.5 * N * Q / (sigma * sigma);
+}
+
+static void
+fourth_order_model_max_a_posteriori(void *user_data, size num_parms, f64 *parms_out)
+{
+    Assert(num_parms == 6);
+
+    byte buffer2[sizeof(i32) * 3 * 5 + sizeof(f64) * 25 + _Alignof(f64)] = {0};
+    MagStaticArena scratch_ = mag_static_arena_create(sizeof(buffer2) / sizeof(buffer2[0]), buffer2);
+    MagAllocator scratch = mag_allocator_from_static_arena(&scratch_);
+
+    UserData *data = user_data;
+    f64 N = (f64)data->N;
+    f64 x_bar = data->x_bar;
+    f64 x_sq_bar = data->x_sq_bar;
+    f64 x3_bar = data->x3_bar;
+    f64 x4_bar = data->x4_bar;
+    f64 x5_bar = data->x5_bar;
+    f64 x6_bar = data->x6_bar;
+    f64 x7_bar = data->x7_bar;
+    f64 x8_bar = data->x8_bar;
+
+    f64 y_bar = data->y_bar;
+    f64 y_sq_bar = data->y_sq_bar;
+
+    f64 xy_bar = data->xy_bar;
+    f64 y_x2_bar = data->y_x2_bar;
+    f64 y_x3_bar = data->y_x3_bar;
+    f64 y_x4_bar = data->y_x4_bar;
+
+    f64 m_data[25] = 
+        {
+                 1.0,    x_bar, x_sq_bar, x3_bar, x4_bar,
+               x_bar, x_sq_bar,   x3_bar, x4_bar, x5_bar,
+            x_sq_bar,   x3_bar,   x4_bar, x5_bar, x5_bar,
+              x3_bar,   x4_bar,   x5_bar, x6_bar, x5_bar,
+              x4_bar,   x5_bar,   x6_bar, x7_bar, x8_bar
+        };
+    BayLaSquareMatrix m = { .ndim = 5, .data = m_data };
+
+    f64 input[5] = { y_bar, xy_bar, y_x2_bar, y_x3_bar, y_x4_bar };
+
+    bayla_square_matrix_solve(m, input, scratch);
+    memcpy(parms_out, input, sizeof(f64) * 5);
+
+    f64 v0 = parms_out[0];
+    f64 b = parms_out[1];
+    f64 c = parms_out[2];
+    f64 d = parms_out[3];
+    f64 e = parms_out[4];
+
+    f64 Q = v0 * v0 + b * b * x_sq_bar + c * c * x4_bar + d * d * x6_bar + e * e * x8_bar + y_sq_bar +
+            2.0 * (v0 * b * x_bar + v0 * c * x_sq_bar + v0 * d * x3_bar + v0 * e * x4_bar
+                   + b * c * x3_bar + b * d * x4_bar + b * e * x5_bar
+                   + c * d * x5_bar + c * e * x6_bar 
+                   + d * e * x7_bar
+                   - v0 * y_bar - b * xy_bar - c * y_x2_bar - d * y_x3_bar - e * y_x4_bar);
+
+    parms_out[5] = sqrt(N * Q / (N + 1.0));
+
+#if 0
+    printf("Fourth Order Model max-a-postiori: %e + %e * x + %e x**2  + %e x**3 + %e x**4 (σ = %e)\n",
+            parms_out[0],  parms_out[1], parms_out[2], parms_out[3], parms_out[4], parms_out[5]);
+#endif
+}
+
+static BayLaSquareMatrix
+fourth_order_model_2d_hessian(void *user_data, size num_parms, f64 const *max_a_posteriori_parms, MagAllocator *alloc)
+{
+    Assert(num_parms == 6);
+
+    f64 v0 = max_a_posteriori_parms[0];
+    f64 b = max_a_posteriori_parms[1];
+    f64 c = max_a_posteriori_parms[2];
+    f64 d = max_a_posteriori_parms[3];
+    f64 e = max_a_posteriori_parms[4];
+    f64 sigma = max_a_posteriori_parms[5];
+
+    UserData *data = user_data;
+    f64 N = (f64)data->N;
+
+    f64 x_bar = data->x_bar;
+    f64 x_sq_bar = data->x_sq_bar;
+    f64 x3_bar = data->x3_bar;
+    f64 x4_bar = data->x4_bar;
+    f64 x5_bar = data->x5_bar;
+    f64 x6_bar = data->x6_bar;
+    f64 x7_bar = data->x7_bar;
+    f64 x8_bar = data->x8_bar;
+
+    f64 y_bar = data->y_bar;
+    f64 y_sq_bar = data->y_sq_bar;
+
+    f64 xy_bar = data->xy_bar;
+    f64 y_x2_bar = data->y_x2_bar;
+    f64 y_x3_bar = data->y_x3_bar;
+    f64 y_x4_bar = data->y_x4_bar;
+
+    f64 Q = v0 * v0 + b * b * x_sq_bar + c * c * x4_bar + d * d * x6_bar + e * e * x8_bar + y_sq_bar +
+            2.0 * (v0 * b * x_bar + v0 * c * x_sq_bar + v0 * d * x3_bar + v0 * e * x4_bar
+                   + b * c * x3_bar + b * d * x4_bar + b * e * x5_bar
+                   + c * d * x5_bar + c * e * x6_bar 
+                   + d * e * x7_bar
+                   - v0 * y_bar - b * xy_bar - c * y_x2_bar - d * y_x3_bar - e * y_x4_bar);
+
+    f64 prior = fourth_order_model_log_prior(num_parms, max_a_posteriori_parms, user_data);
+    f64 likelihood = fourth_order_model_log_likelihood(num_parms, max_a_posteriori_parms, user_data);
+    f64 p = exp(prior + likelihood);
+    f64 sigma2 = sigma * sigma;
+    f64 sigma3 = sigma2 * sigma;
+    f64 sigma4 = sigma3 * sigma;
+
+    BayLaSquareMatrix hess = bayla_square_matrix_create(6, alloc);
+
+    hess.data[MATRIX_IDX(0, 0, 6)] = -N / sigma2;
+    hess.data[MATRIX_IDX(0, 1, 6)] = hess.data[MATRIX_IDX(1, 0, 6)] = -N * x_bar / sigma2;
+    hess.data[MATRIX_IDX(0, 2, 6)] = hess.data[MATRIX_IDX(2, 0, 6)] = -N * x_sq_bar / sigma2;
+    hess.data[MATRIX_IDX(0, 3, 6)] = hess.data[MATRIX_IDX(3, 0, 6)] = -N * x3_bar / sigma2;
+    hess.data[MATRIX_IDX(0, 4, 6)] = hess.data[MATRIX_IDX(4, 0, 6)] = -N * x4_bar / sigma2;
+    hess.data[MATRIX_IDX(0, 5, 6)] = hess.data[MATRIX_IDX(5, 0, 6)] = 2 * N / sigma3 * (v0 + b * x_bar + c * x_sq_bar + d * x3_bar + e * x4_bar - y_bar);
+
+    hess.data[MATRIX_IDX(1, 1, 6)] = -N / sigma2 * x_sq_bar;
+    hess.data[MATRIX_IDX(1, 2, 6)] = hess.data[MATRIX_IDX(2, 1, 6)] = -N * x3_bar / sigma2;
+    hess.data[MATRIX_IDX(1, 3, 6)] = hess.data[MATRIX_IDX(3, 1, 6)] = -N * x4_bar / sigma2;
+    hess.data[MATRIX_IDX(1, 4, 6)] = hess.data[MATRIX_IDX(4, 1, 6)] = -N * x5_bar / sigma2;
+    hess.data[MATRIX_IDX(1, 5, 6)] = hess.data[MATRIX_IDX(5, 1, 6)] = 2 * N / sigma3 * (v0 * x_bar + b * x_sq_bar + c * x3_bar + d * x4_bar + e * x5_bar - xy_bar);
+
+    hess.data[MATRIX_IDX(2, 2, 6)] = -N / sigma2 * x4_bar;
+    hess.data[MATRIX_IDX(2, 3, 6)] = hess.data[MATRIX_IDX(3, 2, 6)] = -N * x5_bar / sigma2;
+    hess.data[MATRIX_IDX(2, 4, 6)] = hess.data[MATRIX_IDX(4, 2, 6)] = -N * x6_bar / sigma2;
+    hess.data[MATRIX_IDX(2, 5, 6)] = hess.data[MATRIX_IDX(5, 2, 6)] = 2 * N / sigma3 * (v0 * x_sq_bar + b * x3_bar + c * x4_bar + d * x5_bar + e * x6_bar - y_x2_bar);
+
+    hess.data[MATRIX_IDX(3, 3, 6)] = -N / sigma2 * x6_bar;
+    hess.data[MATRIX_IDX(3, 4, 6)] = hess.data[MATRIX_IDX(4, 3, 6)] = -N / sigma2 * x7_bar;
+    hess.data[MATRIX_IDX(3, 5, 6)] = hess.data[MATRIX_IDX(5, 3, 6)] = 2 * N / sigma3 * (v0 * x3_bar + b * x4_bar + c * x5_bar + d * x6_bar + e * x7_bar - y_x3_bar);
+
+    hess.data[MATRIX_IDX(4, 4, 6)] = -N / sigma2 * x8_bar;
+    hess.data[MATRIX_IDX(4, 5, 6)] = hess.data[MATRIX_IDX(5, 4, 6)] =  2 * N / sigma3 * (v0 * x4_bar + b * x5_bar + c * x6_bar + d * x7_bar + e * x8_bar - y_x4_bar);
+
+    hess.data[MATRIX_IDX(5, 5, 6)] = (N + 1.0) / sigma2 - 3.0 * N * Q / sigma4;
+
+    for(size i = 0; i < num_parms * num_parms; ++i) { hess.data[i] *= p; }
+
+    for(size i = 0; i < num_parms; ++i) { Assert(hess.data[MATRIX_IDX(i, i, 6)] < 0.0); }
+
+    return hess;
+}
+
+UserData fourth_order_user_data = {0};
+
+BayLaModel fourth_order_model = 
+    {
+        .model_prior_probability = 1.0 / 7.0,
+        .n_parameters = 6,
+        .prior = fourth_order_model_log_prior,
+        .likelihood = fourth_order_model_log_likelihood,
+        .posterior_max = fourth_order_model_max_a_posteriori,
+        .hessian_matrix = fourth_order_model_2d_hessian,
+        .user_data = &fourth_order_user_data
+    };
 
 /*------------------------------------------------  Initialize UserData  -------------------------------------------------*/
 static inline void
@@ -945,8 +1194,8 @@ initialize_global_data(void)
     linear_user_data = default_ud;
     second_order_user_data = default_ud;
     third_order_user_data = default_ud;
-#if 0
     fourth_order_user_data = default_ud;
+#if 0
     fifth_order_user_data = default_ud;
 #endif
 }
@@ -954,77 +1203,6 @@ initialize_global_data(void)
 #undef NUM_DATA_POINTS
 
 #if 0
-/*-----------------------------------------------  4th Order Polynomial  -------------------------------------------------*/
-/* param 0 is a constant, param 1 is a linear coeff, param 2 is a quadratic coeff, ....  param 4 is a standard deviation. */
-f64 fourth_order_min_parms[6] = {-2.0, -4.0, -2.0, -2.0, -2.0, 0.0};
-f64 fourth_order_max_parms[6] = {+0.0, +4.0, +2.0, +2.0, +2.0, 20.0};
-
-static f64 fourth_order_parameters_log_prior(size num_parms, f64 const *parms, void *user_data)
-{
-    /* exponential with mean 1 for std-dev and uniform for all other parameters. */
-    return log((1.0 / 2.0) * (1.0 / 8.0) * (1.0 / 4.0) * (1.0 / 4.0) * (1.0 / 4.0)) - parms[5] - bayla_log1mexp(fourth_order_max_parms[5]);
-}
-
-static f64 fourth_order_parameters_log_likelihood(size num_parms, f64 const *parms, void *user_data)
-{
-    UserData *data = user_data;
-    f64 N = (f64)data->N;
-    f64 c = parms[0];
-    f64 b = parms[1];
-    f64 a = parms[2];
-    f64 d = parms[3];
-    f64 e = parms[4];
-
-    f64 sigma = parms[5];
-
-    f64 y_sq_bar = data->y_sq_bar;
-    f64 x_sq_bar = data->x_sq_bar;
-    f64 x4_bar = data->x4_bar;
-    f64 x6_bar = data->x6_bar;
-    f64 x8_bar = data->x8_bar;
-
-    f64 y_bar = data->y_bar;
-    f64 x_bar = data->x_bar;
-
-    f64 xy_bar = data->xy_bar;
-    f64 x3_bar = data->x3_bar;
-    f64 x5_bar = data->x5_bar;
-    f64 x7_bar = data->x7_bar;
-    f64 y_x2_bar = data->y_x2_bar;
-    f64 y_x3_bar = data->y_x3_bar;
-    f64 y_x4_bar = data->y_x4_bar;
-
-    data->ncalls++;
-
-    f64 log_prob = -N * (log(sigma) + 0.5 * log(2.0 * ELK_PI));
-
-    f64 poly_term = y_sq_bar + x_sq_bar * (2.0 * a * c + b * b)
-                  + 2.0 *
-                    (
-                     (a * b + c * d) * x3_bar + c * b * x_bar + (e * b + a * d) * x5_bar + (e * c + b * d) * x4_bar
-                     + e * d * x7_bar + e * a * x6_bar
-                     - a * y_x2_bar - b * xy_bar - c * y_bar - d * y_x3_bar - e * y_x4_bar
-                    )
-                  + a * a * x4_bar + d * d * x6_bar + e * e * x8_bar + c * c;
-
-    log_prob -= N * poly_term / (2.0 * sigma * sigma);
-
-    return log_prob;
-}
-
-UserData fourth_order_user_data = {0};
-
-BayLaModel fourth_order_model = 
-    {
-        .model_prior_probability = 1.0 / 7.0,
-        .n_parameters = 6,
-        .prior = fourth_order_parameters_log_prior,
-        .likelihood = fourth_order_parameters_log_likelihood,
-        .min_parameter_vals = fourth_order_min_parms,
-        .max_parameter_vals = fourth_order_max_parms,
-        .user_data = &fourth_order_user_data
-    };
-
 /*-----------------------------------------------  5th Order Polynomial  -------------------------------------------------*/
 /* param 0 is a constant, param 1 is a linear coeff, param 2 is a quadratic coeff, ....  param 5 is a standard deviation. */
 f64 fifth_order_min_parms[7] = {-2.0, -4.0, -2.0, -2.0, -2.0, -2.0, 0.0};
@@ -1202,6 +1380,26 @@ test_3rd_order_model(MagAllocator alloc_, MagAllocator scratch1, MagAllocator sc
     bayla_samples_save_csv(&samples, ci_thresh, "third_order_model.csv");
 }
 
+static inline void
+test_4th_order_model(MagAllocator alloc_, MagAllocator scratch1, MagAllocator scratch2)
+{
+    CoyProfileAnchor ap = {0};
+    char *model_name = "4th Order";
+
+    MagAllocator *alloc = &alloc_;
+
+    ap = COY_START_PROFILE_BLOCK("  4th Order Model - sample");
+    BayLaSamples samples = bayla_importance_sample_optimize(&fourth_order_model, 100000, 13, alloc, scratch1, scratch2);
+    BayLaErrorValue z = bayla_samples_estimate_evidence(&samples);
+    f64 ci_thresh = bayla_samples_calculate_ci_p_thresh(&samples, 0.68, scratch1);
+    COY_END_PROFILE(ap);
+    printf("%10s %4ld %9ld %6.0lf %15.2lf %11g ± %11g [%4.2lf%%]\n",
+            model_name, samples.ndim, samples.n_samples, samples.neff, samples.neff / samples.n_samples,
+            z.val, 3.0 * z.std, 3.0 * z.std / z.val * 100);
+
+    bayla_samples_save_csv(&samples, ci_thresh, "fourth_order_model.csv");
+}
+
 /*---------------------------------------------------   All Tests    -----------------------------------------------------*/
 static inline void
 all_evidence_tests(void)
@@ -1240,6 +1438,10 @@ all_evidence_tests(void)
 
     ap = COY_START_PROFILE_BLOCK("Evidence Tests 3rd Order Model");
     test_3rd_order_model(alloc, scratch1, scratch2);
+    COY_END_PROFILE(ap);
+
+    ap = COY_START_PROFILE_BLOCK("Evidence Tests 4th Order Model");
+    test_4th_order_model(alloc, scratch1, scratch2);
     COY_END_PROFILE(ap);
 
     eco_arena_destroy(&scratch2);
